796d76943feb2fdfbd0d21eb7971e5d291c03468
pjcevans <peterjcevans@gmail.com>
2017-04-28 09:46:44 +0100

    tidied up old files, run dev script

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { render } from 'react-dom'
 import routes from './routes'
 import './styles/style.css'
+import Home from "./components/ui/Home"
 
 window.React = React
 
